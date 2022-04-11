021b414e4415add946b369f52ddc3a65387305f5
Carl Jordan Duff <jordan.duff@student.awesomeinc.org>
2022-04-11 20:35:19 +0000

    migrations updated

---
+++ music_library/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from django.db import models
+
+class Artist(models.Model):
+    name = models.CharField(max_length=200, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+
+class Song(models.Model):
+    title = models.CharField(max_length=500)
+    duration = models.IntegerField()
+    artist_name = models.CharField(max_length=200, blank=False, null=False)
+    genre = models.CharField(max_length=200)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE) #many-to-one... one artist can have many songs
+
+    def __str__(self):
+        return (self.title)
